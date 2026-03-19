3fad479f4c494f57aabda08853263b7f946803d2
Todd Wyatt <reclinerhead@gmail.com>
2026-03-19 10:30:59 -0400

    adding vercel analytics package, added tag to layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -28,6 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+    <Analytics />
     </html>
   );
 }
